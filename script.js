217c022aa2b74948aba8d639c73a0711c511883f
Denis <sipulpa@mail.ru>
2019-07-02 10:16:29 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var number = 5;
+var string ="Hello";
+var sym = Symbol();
+var boolean = true; 
+null; 
+undefined;
+var obj ={};
+
+
+console.log(4/0);
+console.log(4*'str');
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Hello World!");
+// let answer = confirm("are you here?");
+// console.log(answer);
+let answer = prompt("Есть ли вам 18?", "Да");
+console.log(typeof(answer));
